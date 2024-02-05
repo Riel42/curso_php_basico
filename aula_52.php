b0bcbba056fb186cd069cc21f45c8347d3c6242c
Riel42 <conta.gabryelrm@gmail.com>
2024-02-05 14:08:52 -0300

    DILATACAO HIPNOTICA 5.0

--- aula_52.php
+++ aula_52.php
@@ -1,5 +1,30 @@
-<?php 
+<?php
 
-	# GETTERS AND SETTERS  #
-	
+        # GETTERS AND SETTERS  #
+        # With this functions, I can set and get private variables on my class
+
+        class Human{
+                private $years_old;
+
+                public function setYearsOld($value){
+                        if(is_numeric($value)){ # if $value is a numeric value:
+                                $this->years_old = $value;
+                        }
+                }
+
+                public function getYearsOld(){
+                        return $this->years_old;
+                }
+        }
+
+        $me = new Human();
+
+        $me->setYearsOld("Hello, world!"); #I can't do this
+        echo $me->getYearsOld() . "<br>";
+
+        $me->setYearsOld(42); #It's possible
+        echo $me->getYearsOld() . "<br>";
+
+
 ?>
+
